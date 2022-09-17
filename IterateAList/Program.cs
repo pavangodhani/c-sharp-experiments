51fe857b1e2d133bfe05a69e5a9f29c44f0ed61b
Pavan <pavangodhani@gmail.com>
2022-09-17 12:00:39 +0530

    Iterate list

--- IterateAList/Program.cs
+++ IterateAList/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Validators;
+
+var config = new ManualConfig()
+        .WithOptions(ConfigOptions.DisableOptimizationsValidator)
+        .AddValidator(JitOptimizationsValidator.DontFailOnError)
+        .AddLogger(ConsoleLogger.Default)
+        .AddColumnProvider(DefaultColumnProviders.Instance);
+
+BenchmarkRunner.Run<Benchmarks.Benchmarks>(config);
